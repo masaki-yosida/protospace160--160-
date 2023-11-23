c660798c5145a860d0563c0c69590290ad093fcf
Yuichi-Nozawa <94278286+Yuichi-Nozawa@users.noreply.github.com>
2023-11-23 18:36:46 +0900

    新規プロトタイプ投稿

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,16 @@
 class Prototype < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  has_one_attached :image
+
+  validate :image_presence
+
+  private
+
+  def image_presence
+    errors.add(:image, 'must be attached') unless image.attached?
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :prototypes
+
   # Remove any attr_accessor for name, profile, occupation, position if present
   validates :name, presence: true
   validates :profile, presence: true
