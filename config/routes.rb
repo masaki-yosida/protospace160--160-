a19ac4e6c60990c25efcbe52f6a1d7ace8b45b71
masaki-yosida <yashukasono@gmail.com>
2023-11-24 00:46:23 +0900

    一覧機能終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create]
+  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :users, only: [:show]
 end
-
