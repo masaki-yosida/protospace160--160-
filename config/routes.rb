bad5361d922b8a79946da5de4a552dfabee8f430
masaki-yosida <yashukasono@gmail.com>
2023-11-24 18:28:59 +0900

    プロトタイプ編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :destroy] # Include :edit here
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show]
 end
