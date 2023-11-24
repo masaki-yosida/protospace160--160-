e155b9bdcbad5a1e174e406c8698b0dd1b319cb0
Yuichi-Nozawa <94278286+Yuichi-Nozawa@users.noreply.github.com>
2023-11-24 18:27:40 +0900

    Revert "反映しない"
    This reverts commit ad8efac994a44058acee0bcd1a4801c538e38747.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,13 @@
 class PrototypesController < ApplicationController
+
   before_action :move_to_index, except: [:index, :show, :destroy]
 
   def index
     @prototypes = Prototype.all
     logger.debug "Prototypes: #{@prototypes.inspect}"
   end
-
+  
+  
   def show
     @prototype = Prototype.find(params[:id])
   end
@@ -30,28 +32,17 @@ class PrototypesController < ApplicationController
     end
   end
 
-  def edit
-    @prototype = Prototype.find(params[:id])
-  end
-
-  def update
-    @prototype = Prototype.find(params[:id])
-    if @prototype.update(prototype_params)
-      redirect_to root_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   private
 
   def prototype_params
+
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    unless user_signed_in? && (params[:action] == 'new' || params[:action] == 'create' || params[:action] == 'edit' || params[:action] == 'update')
+    unless user_signed_in? && (params[:action] == 'new' || params[:action] == 'create')
       redirect_to action: :index
     end
   end
+  
 end
